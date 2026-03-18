80aeff551269527806f87a1fd61ed232c2560561
Willy <willington.londono@ucaldas.edu.co>
2026-03-17 20:46:04 -0500

    Nuevo mundo

--- src/main/java/edu/ucaldas/demo/Application.java
+++ src/main/java/edu/ucaldas/demo/Application.java
@@ -13,7 +13,7 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "GestionProyectos") String name) {
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
 }
